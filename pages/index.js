329bce9a686a2ef002d04230d102961f7a9c72c0
Vitor <109159821+vgsmith@users.noreply.github.com>
2023-12-03 11:01:19 +0000

    adiciona alerta para o play

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ export default function Home() {
     return (
         <div>
             <h1>Oi Raul! 😎 </h1>
+            <h2>Mamãe aperta o play ▶️ </h2>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/8A60RyGY7OQ?autoplay=1&si=ehefjfOVPIa2GPlA&amp;start=25" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>    
         </div>
  
