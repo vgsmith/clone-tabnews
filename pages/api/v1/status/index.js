93bcef4aa16b723534e369d6c91b3c99618d0abc
Vitor Smith <109159821+vgsmith@users.noreply.github.com>
2024-03-01 19:55:42 +0000

    add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(request, response) {
-  //response.status(200).send("alunos do curso.dev são pessoas acima da média");
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as Sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "média" });
 }
 
