c090b5982ece1e79a1c16273dd6bd58053b0dff2
Vitor Smith <109159821+vgsmith@users.noreply.github.com>
2024-03-13 13:48:23 +0000

    change db version test to expect a string

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -8,7 +8,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedDate = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedDate);
 
-  expect(responseBody.dependencies.database.version).toBeGreaterThanOrEqual(16);
+  expect(responseBody.dependencies.database.version).toBe("16.0");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.connections).toEqual(1);
 });
