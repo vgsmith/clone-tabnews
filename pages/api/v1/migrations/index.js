9670afde336099d3eea4a607fefc50b53353fb77
Vitor Smith <109159821+vgsmith@users.noreply.github.com>
2024-05-03 20:01:36 +0000

    add migrations bug the same way course does

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -4,46 +4,34 @@ import database from "infra/database.js";
 
 export default async function migrations(request, response) {
   const dbClient = await database.getNewClient();
+
   const defaultMigrationOptions = {
     dbClient: dbClient,
     dir: join("infra", "migrations"),
     direction: "up",
     verbose: true,
     migrationsTable: "pgmigrations",
+    dryRun: true,
   };
 
-  switch (request.method) {
-    case "GET":
-      const customMigrationOptionsGet = Object.assign(defaultMigrationOptions, {
-        dryRun: true,
-      });
-      const pendingMigrations = await migrationRunner(
-        customMigrationOptionsGet,
-      );
-      await dbClient.end();
-      return response.status(200).json(pendingMigrations);
-      break;
-
-    case "POST":
-      const customMigrationOptionsPost = Object.assign(
-        defaultMigrationOptions,
-        {
-          dryRun: false,
-        },
-      );
-      const migratedMigrations = await migrationRunner(
-        customMigrationOptionsPost,
-      );
-      await dbClient.end();
-      if (migratedMigrations.length > 0) {
-        return response.status(201).json(migratedMigrations);
-      } else {
-        return response.status(200).json(migratedMigrations);
-      }
-      break;
-
-    default:
-      await dbClient.end();
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
   }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+    await dbClient.end();
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    } else {
+      return response.status(200).json(migratedMigrations);
+    }
+  }
+
   return response.status(405).end();
 }
